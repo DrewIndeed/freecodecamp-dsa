f2f9f9f72b9c1c232344c1f3e4f83507e60009d6
Andrew Le <s3820098@rmit.edu.vn>
2022-02-05 09:24:04 +0700

    5. Prototype inheritance
    - Step to create and use inheritance
    - About supertype and subtype

---
+++ 3-OOP/1-inheritance.js
@@ -0,0 +1,55 @@
+// SUPERTYPE
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  describe: function () {
+    console.log('My name is ' + this.name);
+  },
+};
+
+// SUBTYPE
+function Bird(name) {
+  this.name = name;
+}
+
+function Dog(name) {
+  this.name = name;
+}
+
+Bird.prototype = {
+  constructor: Bird,
+};
+
+Dog.prototype = {
+  constructor: Dog,
+};
+
+/*
+Inherit Behaviors from a Supertype
+*/
+// STEP 1: make an instance of the supertype (or parent)
+// Way 1 -> have some disadvantages
+// let animal = new Animal();
+// Way 2 -> resolve advantages
+/*
+- Object.create(obj) -> creates a new Object + set the new Object's prototype to be 'obj'
+- By setting the prototype of animal to be the prototype of Animal, 
+you are giving the animal instance the same "recipe" as any other instance of Animal
+*/
+let animal = Object.create(Animal.prototype);
+console.log(animal instanceof Animal); // true
+
+// STEP 2: prototype of the subtype (or child) to be an instance of Animal
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// INSTANCES OF SUBTYPE
+// testBird inherits all of Animal's properties, including the describe method
+// testDog inherits all of Animal's properties, including the describe method
+let testBird = new Bird('Lalala');
+let testDog = new Dog('Lilili');
+
+// tests 
+testBird.describe();
+testDog.describe();
