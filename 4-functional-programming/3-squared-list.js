2865b0b7819f42006d87b18154c0a96e3e9e15f0
Andrew Le <s3820098@rmit.edu.vn>
2022-02-05 22:24:21 +0700

    11. Create squared list

---
+++ 4-functional-programming/3-squared-list.js
@@ -0,0 +1,8 @@
+const squareList = arr => {
+  // Only change code below this line
+  return arr.filter(item => (item >= 0) && (item - Math.floor(item) === 0)).map(item => item * item);
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
+console.log(squaredIntegers); // [16, 1764, 36]
